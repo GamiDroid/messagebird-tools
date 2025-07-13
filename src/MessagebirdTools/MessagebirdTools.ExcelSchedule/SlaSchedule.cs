1566d9041c8be4dca5b56fe64a52686b911c8c8b
GamiDroid <kevin.d.damen@gmail.com>
2025-07-13 21:43:59 +0200

    Fix naming for messagebird database values `Schedule`

--- src/MessagebirdTools/MessagebirdTools.ExcelSchedule/SlaSchedule.cs
+++ src/MessagebirdTools/MessagebirdTools.ExcelSchedule/SlaSchedule.cs
@@ -1,3 +1,3 @@
 ﻿namespace MessagebirdTools.ExcelSchedule;
 
-public record SlaSchedule(ICollection<Consignee> Consignees, ICollection<Schedule> Schedules);
+public record SlaSchedule(ICollection<Consignee> Consignees, ICollection<Schedule> Schedule);
